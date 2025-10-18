53bc5ebe801ded7fed92e509aeff2d55c1e6be99
bayufadayan <bayufadayan@gmail.com>
2025-10-19 00:39:36 +0700

    feat: implement style.css

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // Do your work here...
 console.log('Hello, world!');
+
+function addBook(item) {
+    
+}
+
+function deleteBook(id) {
+    
+}
+
+function updateBook(id) {
+    
+}
+
+function completeBook(id) {
+    
+}
